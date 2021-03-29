97728fa639cb3b533a344188908ec56232b5b601
Ben Domschot <Bdomschot@gmail.com>
2021-03-29 13:03:51 -0700

    typo fixes

--- user-directory/src/App.js
+++ user-directory/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Filters from "./components/Filter";
+import API from "./utils/API";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  state = {
+    users: [],
+    search: "",
+  };
+
+  componentDidMount() {
+    API.getUsers()
+      .then(res => {
+        this.setState({ users: res.data.results })
+        console.log(res)
+      })
+      .catch(err => console.log(err));
+  }
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    })
+    
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    console.log(this.state.users)
+    const currentStateUser=this.state.users.filter(user => user.name.last.toUpperCase().includes(this.state.search.toUpperCase()) )
+    this.setState({users: currentStateUser})
+  }; 
+
+  handleButtonLastNameSort = event => {
+    event.preventDefault();
+    const sortLast =this.state.users.sort((a,b) => a.name.last > b.name.last ? 1 : -1)
+    this.setState({users: sortLast})
+  };
+
+  handleButtonFirstNameSort = event => {
+    event.preventDefault();
+    const sortFirst =this.state.users.sort((a,b) => a.name.first > b.name.first ? 1 : -1)
+    this.setState({users: sortFirst})
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <Header
+        search={this.state.search}
+        handleFormSubmit={this.handleFormSubmit}
+        handleInputChange={this.handleInputChange} />
+        <Filters 
+        handleButtonLastNameSort={this.handleButtonLastNameSort}
+        handleButtonFirstNameSort={this.handleButtonFirstNameSort} />
+        <Body users={this.state.users} />
     </div>
-  );
-}
-
+    )
+  }
+};
 export default App;
